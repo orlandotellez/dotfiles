26db92fecc7418f9aceb66cb158acbbc71caf2c3
OrlandoTellez <espadachinkinno@gmail.com>
2025-12-20 15:06:49 -0600

    feat: agregar explorador de archivos y carpetas en la parte derecha

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,30 @@
+return {
+  --Configuramos Neo-tree a la derecha
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+      },
+    },
+  },
+
+  --Re-mapeamos el atajo <leader>e para que use Neo-tree y no el otro
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false }, -- Desactivamos el explorador de la izquierda
+    },
+    keys = {
+      -- Esto sobreescribe el atajo de LazyVim para que abra Neo-tree
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({ toggle = true, position = "right" })
+        end,
+        desc = "Explorer Neo-tree (Right)",
+      },
+      { "<leader>E", false },
+    },
+  },
+}
