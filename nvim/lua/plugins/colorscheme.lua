d3c4a41f8a1d4520602652ddfb788ce78805e67c
OrlandoTellez <espadachinkinno@gmail.com>
2025-12-28 22:59:57 -0600

    feat: agregar nuevo tema de color

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,24 @@
 return {
   {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
+    "iibe/gruvbox-high-contrast",
+    name = "gruvbox-high-contrast",
     lazy = false,
     priority = 1000,
     config = function()
-      -- Opciones recomendadas para moonfly
-      vim.g.moonflyTransparent = false -- Cambia a true si quieres transparencia
-      vim.g.moonflyUnderlineMatchParen = true
-      vim.g.moonflyVirtualTextColor = true
+      -- Opciones para gruvbox-high-contrast
+      vim.g.gruvbox_bold = 0
+      vim.g.gruvbox_italic = 0
+      vim.g.gruvbox_transparent_bg = 0
+      vim.g.gruvbox_contrast_dark = "hard"
+      vim.g.gruvbox_sign_column = "bg1"
+      vim.g.gruvbox_number_column = "bg0"
+      vim.g.gruvbox_color_column = "bg1"
+
+      -- Establecer fondo oscuro
+      vim.o.background = "dark"
 
       -- Aplicar el tema
-      vim.cmd([[colorscheme moonfly]])
+      vim.cmd([[colorscheme gruvbox-high-contrast]])
     end,
   },
 }
