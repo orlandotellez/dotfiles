2f5b9a9f8f03314a6167bdcb675c9a3392f6e6ef
OrlandoTellez <espadachinkinno@gmail.com>
2026-02-21 22:56:06 -0600

    feat: agregar estilos para opencode y neotree personalizados

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,11 @@ vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true })
 
 -- Ver la documentación/tipo de lo que hay bajo el cursor (como el hover de VS Code)
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Ver documentación" })
+
+-- OpenCode / floats transparente
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#313342", bg = "NONE" })
+
+-- Neotree personalizado en color blanco
+vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = "#ffffff" })
